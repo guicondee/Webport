f342fa71a932b3f2d9d07893079f21ebadc90beb
guicondee <guicondee@outlook.com>
2022-08-25 20:53:54 -0300

    create scrollReveal for div

---
+++ index.js
@@ -0,0 +1,25 @@
+const scrollReveal = ScrollReveal({
+  origin: 'left',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `.profile .ident, .profile .info-All, .profile .tecs, .profile .experiences, .profile .education
+  `,
+  { interval: 100 }
+)
+
+const redReveal = ScrollReveal({
+  origin: 'right',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+redReveal.reveal(
+  `.widgtes .navbar, .widgtes .cards .card-1
+  `,
+  { interval: 100 }
+)
